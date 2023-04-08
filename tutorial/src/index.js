72d536a8d92918158ab6acb7a39981ab5cc3c3be
rahulk16 <rahul162rk@gmail.com>
2023-04-09 03:54:53 +0530

    initial commit

---
+++ tutorial/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+
+const Greeting = () => {
+  return <h1>My First Component.</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting/>);
