3ecb95e4ca02a2164c539d950947c1d1d7aa2dbc
rahulk16 <rahul162rk@gmail.com>
2023-04-19 01:16:51 +0530

    nested components with css

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import ReactDOM  from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const Greeting = () => {
-  return <h1>My First Component.</h1>
-}
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const Greeting = () => {
+// 	return <h1>My First Component.</h1>;
+// };
 
-root.render(<Greeting/>);
+// const Greeting = () => {
+// 	return (
+// 		<>
+// 			<div>
+// 				<h1>My First Component.</h1>
+// 			</div>
+// 			<h2>Hello World!</h2>
+// 		</>
+// 	);
+// };
+
+const BookList = () => {
+	return (
+		<section>
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
+};
+
+const Book = () => {
+	return (
+		<article>
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
+};
+
+const Image = () => <h2>Image Placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => {
+	return <h4>Author</h4>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
