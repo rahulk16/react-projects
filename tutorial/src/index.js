b4e8aac7321991fa8f82b0a6d668910e485a0805
rahulk16 <rahul162rk@gmail.com>
2023-05-01 21:55:09 +0530

    props in jsx

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,48 +3,23 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-// const Greeting = () => {
-// 	return <h1>My First Component.</h1>;
-// };
-
-// const Greeting = () => {
-// 	return (
-// 		<>
-// 			<div>
-// 				<h1>My First Component.</h1>
-// 			</div>
-// 			<h2>Hello World!</h2>
-// 		</>
-// 	);
-// };
-
 const BookList = () => {
 	return (
 		<section>
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
+			<Book image="Image Placeholder" bookTitle="Book Title" author="Author" />
+			<Book image="Image Placeholder" />
 		</section>
 	);
 };
 
-const Book = () => {
+const Book = (props) => {
 	return (
 		<article>
-			<Image />
-			<Title />
-			<Author />
+			<h2>{props.image}</h2>
+			<h2>{props.bookTitle}</h2>
+			<h4>{props.author}</h4>
 		</article>
 	);
-};
-
-const Image = () => <h2>Image Placeholder</h2>;
-const Title = () => <h2>Book Title</h2>;
-const Author = () => {
-	return <h4>Author</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
