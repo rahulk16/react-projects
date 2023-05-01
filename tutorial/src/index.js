41832164d6849a9325f534920b2ce85182fd7863
rahulk16 <rahul162rk@gmail.com>
2023-05-01 22:10:44 +0530

    simple dynamic props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,16 +3,37 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const detailOne = {
+	image: "Image Placeholder",
+	bookTitle: "Book Title",
+	author: "Author",
+};
+
+const detailTwo = {
+	image: "Image Placeholder Two",
+	bookTitle: "Book Title Two",
+	author: "Author Two",
+};
+
 const BookList = () => {
 	return (
 		<section>
-			<Book image="Image Placeholder" bookTitle="Book Title" author="Author" />
-			<Book image="Image Placeholder" />
+			<Book
+				image={detailOne.image}
+				bookTitle={detailOne.bookTitle}
+				author={detailOne.author}
+			/>
+			<Book
+				image={detailTwo.image}
+				bookTitle={detailTwo.bookTitle}
+				author={detailTwo.author}
+			/>
 		</section>
 	);
 };
 
 const Book = (props) => {
+	console.log(props);
 	return (
 		<article>
 			<h2>{props.image}</h2>
